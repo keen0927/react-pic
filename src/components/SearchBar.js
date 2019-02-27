a81d78859b0639c6c5df975964be20deaa3d2141
KEEN <kyh0927@live.co.kr>
2019-02-28 01:03:13 +0900

    2019 02 28 T.T

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react'
 
 export class SearchBar extends Component {
-    onInputChange(event) {
-        console.log(event.target.value)
+    state = {
+      term: 'Hi there'
+    }
+
+    onFormSubmit = (event) =>  {
+      event.preventDefault();
+      console.log(this.state.term)
+    }
+
+    // onFormSubmit(event) {
+    //   event.preventDefault();
+    //   console.log(this.state.term);
+    // }
+    handlerChange = (event) => {
+      this.setState({
+        term: event.target.value
+      })
     }
 
   render() {
+    console.log(this.state.term);
     return (
       <div className="ui segment">
-          <form className="ui form">
+          <form onSubmit={this.onFormSubmit} className="ui form">
+          {/* <form onSubmit={this.onFormSubmit.bind(this)} className="ui form"> */}
             <div className="field">
-                <label>Image Search</label>
+              <label>Image Search</label>
               <input 
                 type="text" 
-                onChange={this.onInputChange}
+                value={this.state.term}
+                onChange={this.handlerChange}
                 
               />
             </div>
