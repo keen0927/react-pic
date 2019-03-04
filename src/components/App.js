41b01856697724a30ac9e2f1f18f08d1a533314d
KEEN <kyh0927@live.co.kr>
2019-03-04 23:53:01 +0900

    2019 03 04

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import { SearchBar } from './SearchBar';
 
 
 class App extends React.Component {
 
     onSearchSubmit(term) {
-        console.log("term : ",term)
+        
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization: 'Client-ID 04397743cc9fb9dafd66250ea77a69a6f99f005bb6e556345cc94d7668ff0b21'
+            }
+        });
     }
     render() {
         return (
